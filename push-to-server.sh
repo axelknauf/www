8e0c0bb15f05691d5dfaa58decc35c8006c09f6b
Axel Knauf <axel.knauf@posteo.de>
2022-08-29 11:02:29 +0200

    Enhance push to server script

--- push-to-server.sh
+++ push-to-server.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 rsync -av --delete public/ kopfkind@192.168.178.27:~/website-staging/
+ssh kopfkind@192.168.178.27 '~/push-to-apache.sh'
+echo "Sync to https://box.axelknauf.de/ done."
+
+exit 0
 
